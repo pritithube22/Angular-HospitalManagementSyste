0e9652a60c6ed610e6d1a7886c872768007ec810
pritithube22 <thubepriti197@.com>
2023-07-09 19:17:32 +0530

    initial commit

---
+++ appointment/src/main/java/com/example/doclogin/AppointmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.doclogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentApplication.class, args);
+		System.out.println("Welcome to Hospital Management System ");
+	}
+
+}
